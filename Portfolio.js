53806e6c7ca9ea6a74dc5722b838060eb6c97c04
Sara3093 <139939555+Sara3093@users.noreply.github.com>
2023-08-19 19:10:37 +0530

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,8 @@
+var typed= new typed(".text",{
+    Strings:["Frontend Developer", "Data Analyst", "Java Programmer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay:1000,
+    loop: true
+}
+);
